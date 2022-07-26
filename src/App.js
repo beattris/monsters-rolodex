39fb26307f8244b753eb17f029e230eb67e25a95
Beattris <beatriceebirim@gmail.com>
2022-07-26 02:20:00 +0100

    added state and button to change state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: 'Beatrice'
+    }
+  }
+  
   render(){
     return (
       <div className="App">
-        
+        <p>Hi, {this.state.name}</p>
+        <button onClick={() =>{
+          this.setState({name: 'Tris'})
+        }}>change name</button>
       </div>
     );
   }
