224ec23e1f4426d46bf9c2c521b2f2e48b31c874
Beattris <beatriceebirim@gmail.com>
2022-08-04 16:36:50 +0100

    added filter logic to the search bar

--- src/App.js
+++ src/App.js
@@ -6,24 +6,45 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
   // using componentDidMount() to fetch the users API
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((users) => this.setState(() => {
-      return {monsters: users}
-    }, () => {
-      console.log(this.state)
-    }))
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          type="search"
+          className="search-box"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return{ searchField }
+            })
+          }}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
