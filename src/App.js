7b022dc11ead8b51840101b0fc4639e6054771be
Beattris <beatriceebirim@gmail.com>
2022-08-09 01:45:48 +0100

    fetching in the functional component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import { Component } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
+
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
-
 import "./App.css";
 
 const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  });
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
-      {/*<SearchBox
+      <SearchBox
         onChangeHandler={onSearchChange}
         placeholder="search monsters"
         className="monsters-search-box"
       />
-  <CardList monsters={filteredMonsters} />*/}
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 };
